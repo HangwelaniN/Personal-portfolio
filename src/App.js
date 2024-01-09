398ca29c0bf042dff8e6b26c532f79ff8ca094e6
HangwelaniN <hngwekhulu@gmail.com>
2024-01-09 14:15:58 +0200

    removed my app

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import {Container,Nav,Navbar} from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import About  from './About';
+import Projects from './Projects';
+import Home from './Home';
+import Contacts from './Contacts';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+  <div className="App">
+     <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">HangwelaniN</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className=" left me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link  as={Link} to="/Home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/About">About</Nav.Link>
+            <Nav.Link  as={Link} to="/Projects">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/Contacts">Contacts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <div>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/Projects" element={<Projects/>}/>
+        <Route path="/Contacts" element={<Contacts/>} />
+      </Routes>
+      
+    </div>
+ 
+  </div>
+  </BrowserRouter>
+  
+    
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Home(){
+    return (
+        <div>
+            <h1>Home here i am</h1>
+        </div>
+    )
+}
